ec219e08e412a187fb0c976226796ae678b3f513
Steve Perkins <stereosteve@users.noreply.github.com>
2025-05-08 09:54:06 -0400

    poor man migrate

--- src/db/migrations.ts
+++ src/db/migrations.ts
@@ -1,16 +1,19 @@
 import { sql } from './sql'
 
-// todo: just do each item once
-export async function pgMigrate() {
-  await sql`
+// migrations.
+// migration will re-run if the text changes at all
+// make migration idempotent
+// see: https://github.com/graphile/migrate?tab=readme-ov-file#idempotency
+const steps = [
+  sql`
   CREATE TABLE IF NOT EXISTS xmls (
     "source" text not null,
     "xmlUrl" text primary key,
     "messageTimestamp" text not null,
     "createdAt" timestamptz DEFAULT CURRENT_TIMESTAMP
-  );`
+  );`,
 
-  await sql`
+  sql`
   CREATE TABLE IF NOT EXISTS releases (
     "source" text not null,
     "key" text primary key,
@@ -37,9 +40,9 @@ export async function pgMigrate() {
     "numNotCleared" int,
     "prependArtist" boolean
   );
-  `
+  `,
 
-  await sql`
+  sql`
   create table if not exists assets (
     "source" text not null,
     "releaseId" text not null,
@@ -49,9 +52,9 @@ export async function pgMigrate() {
     "fileName" text not null,
     PRIMARY KEY ("source", "releaseId", "ref")
   );
-  `
+  `,
 
-  await sql`
+  sql`
   CREATE TABLE IF NOT EXISTS users (
     "apiKey" text, -- app that user authorized
     "id" text,
@@ -61,16 +64,16 @@ export async function pgMigrate() {
     "password" text,
     primary key ("apiKey", "id")
   );
-  `
+  `,
 
-  await sql`
+  sql`
   create table if not exists "s3markers" (
     "bucket" text primary key,
     "marker" text not null
   );
-  `
+  `,
 
-  await sql`
+  sql`
   create table if not exists "isCleared" (
     "releaseId" text not null,
     "trackId" text not null,
@@ -78,12 +81,41 @@ export async function pgMigrate() {
     "isCleared" boolean,
     PRIMARY KEY ("releaseId", "trackId")
   );
-  `
+  `,
 
-  await sql`
+  sql`
   create table if not exists "lsrLog" (
     "file" text primary key,
     "ts" timestamptz not null
   );
+  `,
+
+  sql`create table if not exists cats (
+    "name" text primary key,
+    "breed" text,
+    "good_cat" boolean
+  )`,
+]
+
+// poor man's migrate
+export async function pgMigrate() {
+  await sql`
+  create table if not exists pmigrate (
+    raw text primary key,
+    ran_at timestamptz default now()
+  )
   `
+
+  for (const step of steps) {
+    const describe = await step.describe()
+    const raw = describe.string
+    const [exists] = await sql`select ran_at from pmigrate where raw = ${raw}`
+    if (!exists) {
+      console.log('running', raw)
+      await sql.begin(async (tx) => {
+        await tx.unsafe(raw)
+        await tx`insert into pmigrate values (${raw}, now())`
+      })
+    }
+  }
 }
